865a4117af1599fba0ce791d14872eb12237f815
akbarkhojayev <akbarkhojayev@gmail.com>
2025-03-18 09:45:29 +0500

    Yangilanish haqida qisqa izoh

--- core/urls.py
+++ core/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += [
